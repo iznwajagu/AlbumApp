1c19cc83177c88fc96b7723ddb27911f055baabd
iznwajagu <znwajagu@gmail.com>
2019-02-18 14:54:46 +0000

    Get album list Test Skeleton

---
+++ app/src/androidTest/java/com/zubizaza/albumapp/AlbumViewModelTest.java
@@ -0,0 +1,19 @@
+package com.zubizaza.albumapp;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class AlbumViewModelTest {
+
+    @Test
+    public void populateDatabaseThenGetAlbumList() throws Exception{
+
+
+    }
+
+
+
+}
