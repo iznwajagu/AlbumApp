0119b1a631b374858010a1a5583849eea1c03435
iznwajagu <znwajagu@gmail.com>
2019-02-18 17:18:33 +0000

    subscribe for network errors

--- app/src/main/java/com/zubizaza/albumapp/viewmodels/AlbumViewModel.java
+++ app/src/main/java/com/zubizaza/albumapp/viewmodels/AlbumViewModel.java
@@ -1,6 +1,7 @@
 package com.zubizaza.albumapp.viewmodels;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.zubizaza.albumapp.data.AlbumRepository;
@@ -12,10 +13,12 @@ public class AlbumViewModel extends ViewModel {
 
     private AlbumRepository albumRepository;
     private LiveData<List<Album>> mAlbumList;
+    public MutableLiveData<String> statusMessage = new MutableLiveData<>();
 
     AlbumViewModel(AlbumRepository albumRepository){
         this.albumRepository = albumRepository;
         subscribeToRepositoryAlbums();
+        subscribeToRepoErrors();
     }
 
     void subscribeToRepositoryAlbums(){
@@ -24,6 +27,14 @@ public class AlbumViewModel extends ViewModel {
 
     public LiveData<List<Album>> publishAlbumsToSubsribers() {
         return mAlbumList;
+    }
+
+    private void subscribeToRepoErrors(){
+
+        albumRepository.networkErrors.observeForever( error -> {
+            statusMessage.postValue(error.toString());
+        });
+
     }
 
 
