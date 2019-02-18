33b11a04d2d98dd68413f5e0ef1b9e3a3b81ec54
iznwajagu <znwajagu@gmail.com>
2019-02-18 16:07:52 +0000

    integrate Room and Data Access Object

---
+++ app/src/main/java/com/zubizaza/albumapp/data/AlbumDao.java
@@ -0,0 +1,22 @@
+package com.zubizaza.albumapp.data;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.zubizaza.albumapp.data.model.Album;
+
+import java.util.List;
+
+@Dao
+public interface AlbumDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(List<Album> album);
+
+    @Query("SELECT id, userId, title FROM albumdb")
+    LiveData<List<Album>> fetchAlbums();
+
+}
---
+++ app/src/main/java/com/zubizaza/albumapp/data/AlbumDatabase.java
@@ -0,0 +1,33 @@
+package com.zubizaza.albumapp.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.zubizaza.albumapp.data.model.Album;
+
+@Database(entities = {Album.class}, version = 1 , exportSchema = false)
+public abstract class AlbumDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = AlbumDatabase.class.getSimpleName();
+    private static final String DATABASE_NAME = "albumdb";
+
+
+    private static final Object LOCK = new Object();
+    private static AlbumDatabase sInstance;
+
+    public static AlbumDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AlbumDatabase.class, AlbumDatabase.DATABASE_NAME).build();
+            }
+        }
+        return sInstance;
+    }
+
+    public abstract AlbumDao albumDao();
+
+
+}
