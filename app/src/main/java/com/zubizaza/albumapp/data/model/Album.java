1c91716f4db9095ee601d4baf870327c03c9cb28
iznwajagu <znwajagu@gmail.com>
2019-02-18 16:05:11 +0000

    annotate Album class for Room Integration

--- app/src/main/java/com/zubizaza/albumapp/data/model/Album.java
+++ app/src/main/java/com/zubizaza/albumapp/data/model/Album.java
@@ -1,7 +1,15 @@
 package com.zubizaza.albumapp.data.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "albumdb", indices = {@Index(value = {"id"}, unique = true)})
 public class Album {
 
+    @PrimaryKey
+    @NonNull
     private String userId;
     private String id;
     private String title;
