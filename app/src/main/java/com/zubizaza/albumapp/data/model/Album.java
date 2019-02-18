1ffb74b3509023675d3c0251742821a331d02532
iznwajagu <znwajagu@gmail.com>
2019-02-18 15:28:43 +0000

    add POJO

---
+++ app/src/main/java/com/zubizaza/albumapp/data/model/Album.java
@@ -0,0 +1,41 @@
+package com.zubizaza.albumapp.data.model;
+
+public class Album {
+
+    private String userId;
+    private String id;
+    private String title;
+
+    public Album(String userId, String id, String title) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+}
